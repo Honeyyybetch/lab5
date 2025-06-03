56bca2c9fb50b1b66d839cc71df458bb1fbd2e62
Nastia Tsan <anastasiia.tsan.oi.2023@lpnu.ua>
2025-06-03 04:08:26 +0300

    Fix Loader import

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
-import Loader from './Loader';
+import Loader from './loader';
 
 const ProtectedRoute = ({ children }) => {
   const [user, loading] = useAuthState(auth);
